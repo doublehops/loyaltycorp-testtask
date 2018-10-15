ca955582e1cc3eeede2f2de57d3c69f22f43bc4e
Damien Buttler <damien@doublehops.com>
2018-10-15 20:56:45 +1100

    Adding member to API and Mailchimp.

---
+++ app/Http/Controllers/MailChimp/MembersController.php
@@ -0,0 +1,170 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\MailChimp;
+
+use App\Database\Entities\MailChimp\MailChimpMember;
+use App\Http\Controllers\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Mailchimp\Mailchimp;
+
+class MembersController extends Controller
+{
+    /**
+     * @var \Mailchimp\Mailchimp
+     */
+    private $mailChimp;
+
+    /**
+     * MembersController constructor.
+     *
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     * @param \Mailchimp\Mailchimp $mailchimp
+     */
+    public function __construct(EntityManagerInterface $entityManager, Mailchimp $mailchimp)
+    {
+        parent::__construct($entityManager);
+
+        $this->mailChimp = $mailchimp;
+    }
+
+    /**
+     * Create MailChimp member.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request): JsonResponse
+    {
+        // Instantiate entity
+        $member = new MailChimpMember($request->all());
+        // Validate entity
+        $validator = $this->getValidationFactory()->make($member->toMailChimpArray(), $member->getValidationRules());
+
+        if ($validator->fails()) {
+            // Return error response if validation failed
+            return $this->errorResponse([
+                'message' => 'Invalid data given',
+                'errors' => $validator->errors()->toArray()
+            ]);
+        }
+
+        try {
+            // Save member into db
+            //$this->saveEntity($member->setMailChimpId('tempid')); // @todo find cleaner way to do this
+            $this->saveEntity($member);
+            // Save member into MailChimp
+            $response = $this->mailChimp->post('lists/1a9401e1af/members', $member->toMailChimpArray());
+            // Set MailChimp id on the member and save member into db
+            $this->saveEntity($member->setMailChimpId($response->get('id')));
+        } catch (Exception $exception) {
+            // Return error response if something goes wrong
+            return $this->errorResponse(['message' => $exception->getMessage()]);
+        }
+
+        return $this->successfulResponse($member->toArray());
+    }
+
+    /**
+     * Remove MailChimp member.
+     *
+     * @param string $memberId
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function remove(string $memberId): JsonResponse
+    {
+        /** @var \App\Database\Entities\MailChimp\MailChimpMember|null $member */
+        $member = $this->entityManager->getRepository(MailChimpMember::class)->find($memberId);
+
+        if ($member === null) {
+            return $this->errorResponse(
+                ['message' => \sprintf('MailChimpMember[%s] not found', $memberId)],
+                404
+            );
+        }
+
+        try {
+            // Remove member from database
+            $this->removeEntity($member);
+            // Remove member from MailChimp
+            $this->mailChimp->delete(\sprintf('members/%s', $member->getMailChimpId()));
+        } catch (Exception $exception) {
+            return $this->errorResponse(['message' => $exception->getMessage()]);
+        }
+
+        return $this->successfulResponse([]);
+    }
+
+    /**
+     * Retrieve and return MailChimp member.
+     *
+     * @param string $memberId
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(string $memberId): JsonResponse
+    {
+        /** @var \App\Database\Entities\MailChimp\MailChimpMember|null $member */
+        $member = $this->entityManager->getRepository(MailChimpMember::class)->find($memberId);
+
+        if ($member === null) {
+            return $this->errorResponse(
+                ['message' => \sprintf('MailChimpMember[%s] not found', $memberId)],
+                404
+            );
+        }
+
+        return $this->successfulResponse($member->toArray());
+    }
+
+    /**
+     * Update MailChimp member.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param string $memberId
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, string $memberId): JsonResponse
+    {
+        /** @var \App\Database\Entities\MailChimp\MailChimpMember|null $member */
+        $member = $this->entityManager->getRepository(MailChimpMember::class)->find($memberId);
+
+        if ($member === null) {
+            return $this->errorResponse(
+                ['message' => \sprintf('MailChimpMember[%s] not found', $memberId)],
+                404
+            );
+        }
+
+        // Update member properties
+        $member->fill($request->all());
+
+        // Validate entity
+        $validator = $this->getValidationFactory()->make($member->toMailChimpArray(), $member->getValidationRules());
+
+        if ($validator->fails()) {
+            // Return error response if validation failed
+            return $this->errorResponse([
+                'message' => 'Invalid data given',
+                'errors' => $validator->errors()->toArray()
+            ]);
+        }
+
+        try {
+            // Update member into database
+            $this->saveEntity($member);
+            // Update member into MailChimp
+            $this->mailChimp->patch(\sprintf('members/%s', $member->getMailChimpId()), $member->toMailChimpArray());
+        } catch (Exception $exception) {
+            return $this->errorResponse(['message' => $exception->getMessage()]);
+        }
+
+        return $this->successfulResponse($member->toArray());
+    }
+}
---
+++ database/migrations/2018_10_15_094838_members.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Members extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail_chimp_members', function (Blueprint $table) {
+            //$table->increments('id');
+            $table->string('email_address');
+            $table->string('email_type');
+            $table->uuid('id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail_chimp_members');
+    }
+}
